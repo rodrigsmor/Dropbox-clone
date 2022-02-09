191880f2f1354ea27e20ec7947713f47d12892b1
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-02-09 09:30:10 -0300

    Aula 07 - Exibe progresso do envio

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -3,6 +3,10 @@ class DropBoxController {
         this.btnSendFilesEl = document.querySelector('#btn-send-file');
         this.inputFilesEl = document.querySelector('#files');
         this.snackModalEl = document.querySelector('#react-snackbar-root');
+        this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg');
+        this.nameFileEl = this.snackModalEl.querySelector('.filename');
+        this.timeleftEl = this.snackModalEl.querySelector('.timeleft');
+
         this.initEvents();
     }
 
@@ -12,10 +16,14 @@ class DropBoxController {
         });
 
         this.inputFilesEl.addEventListener('change', event => {
-            console.log(event.target.files);
             this.uploadTask(event.target.files);
-            this.snackModalEl.style.display = 'block';
+            this.modalShow();
+            this.inputFilesEl.value = '';
         });
+    }
+
+    modalShow(show = true) {
+        this.snackModalEl.style.display = (show) ? 'block' : 'none';
     }
 
     uploadTask(files) {
@@ -28,6 +36,8 @@ class DropBoxController {
                 ajax.open('POST', '/upload');
 
                 ajax.onload = event => {
+                    this.modalShow(false);
+
                     try {
                         resolve(JSON.parse(ajax.responseText));
                     } catch(e) {
@@ -36,17 +46,50 @@ class DropBoxController {
                 }
 
                 ajax.onerror = event => {
+                    this.modalShow(false);
                     reject(event);
+                }
+
+                ajax.upload.onprogress = event => {
+                    this.uploadProgress(event, file);
                 }
 
                 let formData = new FormData();
 
                 formData.append('input-file', file);
 
+                this.startUploadTime = Date.now();
+
                 ajax.send(formData);
             }))
         });
 
         return Promise.all(promises);
+    }
+
+    uploadProgress(event, file) {
+        let timespent = Date.now() - this.startUploadTime;
+        let loaded = event.loaded;
+        let total = event.total;
+        let porcent = parseInt((loaded / total) * 100);
+        let timeleft = ((100 - porcent) * timespent) / porcent;
+
+        this.progressBarEl.style.width = `${porcent}%`;
+
+        this.nameFileEl.innerHTML = file.name;
+        this.timeleftEl.innerHTML = this.formatTimeToHuman(timeleft);
+    }
+
+    formatTimeToHuman(duration) {
+        let seconds = parseInt((duration / 100) % 60);
+        let minutes = parseInt((duration / (100 * 60)) % 60);
+        let hours = parseInt((duration / (100 * 60 * 60)) % 24);
+        
+        if(hours > 0)
+            return `${hours} horas, ${minutes} minutos e ${seconds} segundos`;
+        else if(minutes > 0)
+            return `${minutes} minutos e ${seconds} segundos`;
+        else
+            return `${seconds} segundos`;
     }
 }
