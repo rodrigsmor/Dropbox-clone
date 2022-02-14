e5ff913e35c3f24c45d090d6c44b60e8de5e4140
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-02-14 10:35:21 -0300

    Aula 22 - Exclui arquivos do Firebase Storage

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -45,16 +45,18 @@ class DropBoxController {
             let file =  JSON.parse(li.dataset.file);
             let key = li.dataset.key;
     
-            let formData = new FormData()
-    
-            formData.append('path', file.filepath);
-            formData.append('key', key);
-    
-            promises.push(this.ajax(
-                '/file', 
-                'DELETE', 
-                formData
-            ));
+            promises.push(new Promise((resolve, reject) => {
+                let fileRef = firebase.storage().ref(this.currentFolder.join('/')).child(file.originalFilename);
+                fileRef.delete().then(() => {
+                    resolve({
+                        fields: {
+                            key
+                        }
+                    });
+                }).catch(err => {
+                    reject(err);
+                });
+            }));
         });
 
         return Promise.all(promises);
@@ -165,7 +167,7 @@ class DropBoxController {
     }
 
     ajax(
-        url, 
+        url,
         method = 'GET', 
         formData = new FormData(), 
         onprogress = function(){}, 
